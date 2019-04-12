98c4fc9353438003e943a86926fd05b94428d684
Constantin DLR <cdlr75@gmail.com>
2019-04-12 19:36:28 +0200

    DEV: Add wall ENH: Items management

--- babaisyou/app.py
+++ babaisyou/app.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from gui.curses import Curses
 from items import *
 
 logger = logging.getLogger(__name__)
@@ -9,22 +10,26 @@ class GameMap:
     """ GameMap is a matrice """
 
     def __init__(self, width=24, height=24):
-        self.maps = [[None for _ in range(width)] for _ in range(height)]
+        self.width = width
+        self.height = height
+        self.maps = self.empty_map
 
     @property
-    def height(self):
-        return len(self.maps[0])
+    def empty_map(self):
+        return [[None for _ in range(self.width)] for _ in range(self.height)]
 
-    @property
-    def width(self):
-        return len(self.maps)
+    def set_items(self, items):
+        """ Update map with given items """
+        self.maps = self.empty_map
+        for item in items:
+            self.maps[item.posx][item.posy] = item
 
-    def get_entities(self, cls):
-        """ List of entities with their position """
-        return [(el, x, y)
+    def get_items(self, cls):
+        """ List of items on map """
+        return [item
                 for x, col in enumerate(self.maps)
-                for y, el in enumerate(col)
-                if isinstance(el, cls)]
+                for y, item in enumerate(col)
+                if isinstance(item, cls)]
 
     def debug(self):
         import pprint
@@ -35,8 +40,9 @@ class GameMap:
 class App:
     """ App contain rules and handle GUI """
 
-    def __init__(self, gui, game_map):
+    def __init__(self, gui, items, game_map):
         self.gui = gui
+        self.items = items
         self.game_map = game_map
         self._close_future = asyncio.Future()
 
@@ -46,14 +52,15 @@ class App:
 
         :returns: App
         """
-        from gui.curses import Curses
         game_map = GameMap()
-        baba = Baba()
-        flag = Flag()
-        game_map.maps[1][1] = baba
-        game_map.maps[3][3] = flag
+        items = [
+            Baba(1, 1),
+            Wall(1, 2),
+            Flag(3, 3)
+        ]
+        game_map.set_items(items)
         gui = Curses(game_map)
-        app = cls(gui, game_map)
+        app = cls(gui, items, game_map)
         gui.register_actions(app.quit,
                              app.move_up,
                              app.move_down,
@@ -64,15 +71,21 @@ class App:
     def do_move(self, move):
         """ apply move and rules """
         is_win = False
-        flags = self.game_map.get_entities(Flag)
-        for you, src, dst in move:
-            s_x, s_y = src
-            d_x, d_y = dst
-            if any([d_x == x and d_y == y for _, x, y in flags]):
+        for you, d_x, d_y in move:
+            entity = self.game_map.maps[d_x][d_y]
+            if entity is None:
+                # all move
+                you.posx = d_x
+                you.posy = d_y
+            elif isinstance(entity, Flag):
+                # move and win
+                you.posx = d_x
+                you.posy = d_y
                 is_win = True
-                continue
-            self.game_map.maps[s_x][s_y] = None
-            self.game_map.maps[d_x][d_y] = you
+            elif isinstance(entity, Wall):
+                # do not move
+                pass
+        self.game_map.set_items(self.items)
         if is_win:
             logger.info("You win !")
             self.quit()
@@ -85,26 +98,26 @@ class App:
     def move_up(self):
         """ The user want to move """
         logger.debug("move_up")
-        self.do_move([(baba, (x, y), (x, (y-1) % self.game_map.height))
-                      for baba, x, y in self.game_map.get_entities(Baba)])
+        self.do_move([(baba, baba.posx, (baba.posy-1) % self.game_map.height)
+                      for baba in self.game_map.get_items(Baba)])
 
     def move_down(self):
         """ The user want to move """
         logger.debug("move_down")
-        self.do_move([(baba, (x, y), (x, (y+1) % self.game_map.height))
-                      for baba, x, y in self.game_map.get_entities(Baba)])
+        self.do_move([(baba, baba.posx, (baba.posy+1) % self.game_map.height)
+                      for baba in self.game_map.get_items(Baba)])
 
     def move_left(self):
         """ The user want to move """
         logger.debug("move_left")
-        self.do_move([(baba, (x, y), ((x-1) % self.game_map.width, y))
-                      for baba, x, y in self.game_map.get_entities(Baba)])
+        self.do_move([(baba, (baba.posx-1) % self.game_map.width, baba.posy)
+                      for baba in self.game_map.get_items(Baba)])
 
     def move_right(self):
         """ The user want to move """
         logger.debug("move_right")
-        self.do_move([(baba, (x, y), ((x+1) % self.game_map.width, y))
-                      for baba, x, y in self.game_map.get_entities(Baba)])
+        self.do_move([(baba, (baba.posx+1) % self.game_map.width, baba.posy)
+                      for baba in self.game_map.get_items(Baba)])
 
     async def start(self):
         """ Start the App """
--- babaisyou/items.py
+++ babaisyou/items.py
@@ -1,13 +1,20 @@
 """ Items used in game """
 
 
-class Baba:
+class Item:
+    """ Item of the game """
+    def __init__(self, posx, posy):
+        self.posx = posx
+        self.posy = posy
+
+
+class Baba(Item):
     pass
 
 
-class Flag:
+class Flag(Item):
     pass
 
 
-class Wall:
+class Wall(Item):
     pass
