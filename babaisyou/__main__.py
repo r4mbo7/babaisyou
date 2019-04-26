01f40fe0c33f68488495898d2ec1e92b257691bb
Constantin DLR <cdlr75@gmail.com>
2019-04-26 19:40:01 +0200

    MAINT: Add default redis url in args

--- babaisyou/__main__.py
+++ babaisyou/__main__.py
@@ -7,18 +7,26 @@ import logging
 
 
 async def start_server(env):
-    logging.debug("starting server")
+    logging.info("starting server")
     from babaisyou.server.app import Server
     server = await Server.create(env["server"])
     return server
 
 
 async def start_client(env):
-    logging.debug("starting client")
+    logging.info("starting client")
     from babaisyou.server.app import Client
     client = await Client.create(env["maps"], env["client"])
     await client.start()
     return client
+
+
+async def start_solo(env):
+    logging.info("starting solo")
+    from babaisyou.app import App
+    app = await App.create(env["maps"])
+    await app.start()
+    return app
 
 
 async def run(env):
@@ -27,26 +35,22 @@ async def run(env):
     elif env.get("client"):
         return await start_client(env)
     else:
-        logging.debug("starting solo")
-        from babaisyou.app import App
-        app = await App.create(env["maps"])
-        await app.start()
-        return app
+        return await start_solo(env)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--maps', help='Map name', default="maps/default.txt")
-    parser.add_argument('-s', '--server', help='Run server')
-    parser.add_argument('-c', '--client', help='Join server')
+    parser.add_argument('-s', '--server', nargs='?', const="redis://localhost",
+                        help='Start server --server <redis url>')
+    parser.add_argument('-c', '--client', nargs='?', const="redis://localhost",
+                        help='Join server --client <redis url>')
     args = parser.parse_args()
     env = vars(args)
 
     logging.basicConfig(filename='app.log', level=logging.DEBUG)
-    logging.info('Game on !')
 
     loop = asyncio.get_event_loop()
-
     app = loop.run_until_complete(run(env))
 
     def shutdown(sig):
